006c031b6ac02ca2595f963b80521bbb989a78f2
Duzo <james.hall01@outlook.com>
2024-01-21 23:44:05 +0000

    initial commit with a blank mod

---
+++ src/main/java/mc/duzo/timeless/TimelessHeroes.java
@@ -0,0 +1,16 @@
+package mc.duzo.timeless;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TimelessHeroes implements ModInitializer {
+	public static final String MODID = "timeless";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
