273b58c5a5e39bdb5b6d5f24ceaad49ffd8059a9
James Hall <james.hall01@outlook.com>
2024-08-26 01:29:02 +0100

    chore: move to 1.20.1

--- src/main/java/mc/duzo/TimelessDataGenerator.java
+++ src/main/java/mc/duzo/TimelessDataGenerator.java
@@ -1,9 +1,9 @@
-package mc.duzo.timeless.datagen;
+package mc.duzo;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class TimelessHeroesDataGenerator implements DataGeneratorEntrypoint {
+public class TimelessDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
