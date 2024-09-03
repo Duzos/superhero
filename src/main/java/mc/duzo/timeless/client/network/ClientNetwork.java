3874bf1732ae1bd4ee69ac8b2921f83dd68e4c17
James Hall <james.hall01@outlook.com>
2024-09-03 15:15:57 +0100

    chore: remove old packets fix: imports

--- src/main/java/mc/duzo/timeless/client/network/ClientNetwork.java
+++ src/main/java/mc/duzo/timeless/client/network/ClientNetwork.java
@@ -2,14 +2,10 @@ package mc.duzo.timeless.client.network;
 
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 
-import mc.duzo.timeless.network.s2c.MarkFiveCaseS2CPacket;
-import mc.duzo.timeless.network.s2c.MarkFiveMaskS2CPacket;
 import mc.duzo.timeless.network.s2c.UpdateFlyingStatusS2CPacket;
 
 public class ClientNetwork {
     static {
-        ClientPlayNetworking.registerGlobalReceiver(MarkFiveCaseS2CPacket.TYPE, MarkFiveCaseS2CPacket::handle);
-        ClientPlayNetworking.registerGlobalReceiver(MarkFiveMaskS2CPacket.TYPE, MarkFiveMaskS2CPacket::handle);
         ClientPlayNetworking.registerGlobalReceiver(UpdateFlyingStatusS2CPacket.TYPE, UpdateFlyingStatusS2CPacket::handle);
     }
 
